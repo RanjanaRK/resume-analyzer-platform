3dd8d580991d2ecb3b90af7edca88e300ea62452
RanjanaRK <ranjanakumari.me@gmail.com>
2026-07-07 00:05:01 +0530

    Update auth.route.ts

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import {
+  getAccessTokenController,
   login,
+  logout,
   register,
   verifyEmail,
 } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
+authRouter.get("/verify-email", verifyEmail);
+
+authRouter.get("/get-access-token", getAccessTokenController);
+
 authRouter.post("/register", register);
 
 authRouter.post("/login", login);
 
-authRouter.get("/verify-email", verifyEmail);
+authRouter.post("/logout", logout);
 
 export default authRouter;
